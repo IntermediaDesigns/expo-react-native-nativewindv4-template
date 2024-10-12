45eb91b30a45b14607714ea1dda4a4ef7aa31eb0
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-10-12 11:39:04 -0400

    Refactor JSX syntax in app/index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,11 @@ import { Text, View } from 'react-native'
 export default function Index () {
   return (
     <View>
-      <div className='flex items-center justify-center h-screen'>
+      <View className='flex items-center justify-center h-screen'>
         <Text className='text-center text-4xl'>
           Edit app/index.tsx to edit this screen.
         </Text>
-      </div>
+      </View>
     </View>
   )
 }
